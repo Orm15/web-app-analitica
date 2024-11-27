4694649d24c4c62f0ea0ae69806ac7eb4f9ff6fc
Victor <20100835@ue.edu.pe>
2024-11-27 01:59:28 -0500

    modificando puerto de lanzamiento, amen

--- app.py
+++ app.py
@@ -268,4 +268,6 @@ def subir_pdf():
     return '', 204
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    # Ejecuta en 0.0.0.0 para que Render pueda acceder
+    app.run(host="0.0.0.0", port=port)
